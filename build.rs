fd6ec0226bdc39de75eed096475eca7e4630b12b
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2017-06-27 23:34:05 +0200

    refactor: update code to compile again

--- build.rs
+++ build.rs
@@ -14,5 +14,12 @@ fn main() {
     let completiondir = Path::new(&outdir).join("zsh");
     let mut cli = build_cli();
 	println!("Will install ZSH completion to {}", completiondir.to_string_lossy());
+
     cli.gen_completions("gpxanalyzer", Shell::Zsh, &completiondir);
+
+    // also install in output folder
+    cli.gen_completions("gpxanalyzer", Shell::Bash, &env::var_os("OUT_DIR").unwrap());
+    cli.gen_completions("gpxanalyzer", Shell::Fish, &env::var_os("OUT_DIR").unwrap());
+    cli.gen_completions("gpxanalyzer", Shell::Zsh, &env::var_os("OUT_DIR").unwrap());
 }
+
